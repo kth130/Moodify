e6a5c4aa013923eab2bf9ab139c9e70500f49ef2
Kristján Petur Þórarinsson <kth130@hi.is>
2018-11-20 12:22:47 +0000

    Progress

--- app/src/main/java/hi/is/notspotify/moodify/HomeActivity.java
+++ app/src/main/java/hi/is/notspotify/moodify/HomeActivity.java
@@ -2,6 +2,7 @@ package hi.is.notspotify.moodify;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Spinner spinner_mood;
+        spinner_mood = (Spinner) findViewById(R.id.s_mood);
     }
 }
