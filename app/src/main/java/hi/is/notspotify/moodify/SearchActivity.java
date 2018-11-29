349f846b9041f8803e1893732d81192ea43a7683
Kristján Petur Þórarinsson <kth130@hi.is>
2018-11-29 13:00:14 +0000

    Update search

--- app/src/main/java/hi/is/notspotify/moodify/SearchActivity.java
+++ app/src/main/java/hi/is/notspotify/moodify/SearchActivity.java
@@ -2,6 +2,7 @@ package hi.is.notspotify.moodify;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Spinner;
 
 public class SearchActivity extends AppCompatActivity {
@@ -9,5 +10,14 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+    }
+
+    public void addSong(View v){
+        String number = String.valueOf(v.getTag());
+        String song = "Song " + number;
+        String artist = "Artist";
+        ((HomeActivity)getParent()).addSong(song, artist);
+
+
     }
 }
